0340b70c949d1e99f44eb341d7a9fe9808a85bb1
samdeupta <gupta.samprit2006@gmail.com>
2026-05-14 13:35:17 -0700

    feat: Add database schema for Subjects and Courses tables

---
+++ backend/db/schema.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from sqlalchemy import String, Integer, Numeric, ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+# ----- Base --------------------
+class Base(DeclarativeBase):
+    pass
+
+
+# ----- Tables --------------------
+class Subject(Base):
+    """
+    Stores subject code and name of each UVic subject.
+    """
+    __tablename__ = "Subjects"
+
+    code : Mapped[str]  = mapped_column(String,  primary_key=True)
+    name : Mapped[str]  = mapped_column(String,  nullable=False)
+
+
+class Course(Base):
+    """
+    Stores the course code, subject, level, name, credits, and prereq tree of each UVic course.
+    """
+    __tablename__ = "Courses"
+
+    code            : Mapped[str]           = mapped_column(String,  primary_key=True)
+    subject         : Mapped[str]           = mapped_column(String,  nullable=False)
+    lvl             : Mapped[int]           = mapped_column(Integer, nullable=False)
+    name            : Mapped[str]           = mapped_column(String,  nullable=False)
+    credits         : Mapped[float]         = mapped_column(Numeric, nullable=False)
+    prereq_tree     : Mapped[dict | None]   = mapped_column(JSONB,   nullable=True)
