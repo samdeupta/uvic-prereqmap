9db7d69c7820c6c18a2c4a00afaebbb0a49518a3
samdeupta <gupta.samprit2006@gmail.com>
2026-05-17 18:08:46 -0700

    fix: Update Course Info API and update main entry point for API execution

--- backend/apis/start_all.py
+++ backend/apis/start_all.py
@@ -1,13 +1,12 @@
-import asyncio
+import uvicorn
 
 from course_info_api.src.main import CourseInfoAPI
 
 
-async def main():
-    await asyncio.gather(
-        CourseInfoAPI.create()
-    )
+def main():
+    course_info_api = CourseInfoAPI.create()
+    uvicorn.run(course_info_api, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
