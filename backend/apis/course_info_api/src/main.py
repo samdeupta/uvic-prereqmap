d638a7c2bd270793fdee3887a41a143fded4e28a
samdeupta <gupta.samprit2006@gmail.com>
2026-06-17 20:21:01 -0700

    fix: Add CORS middleware configuration for CourseInfoAPI

--- backend/apis/course_info_api/src/main.py
+++ backend/apis/course_info_api/src/main.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db.connection import db
 from .routes import router as courses_router
@@ -29,6 +29,13 @@ class CourseInfoAPI:
             description = "API for fetching UVic course and prerequisite data.",
             version     = "1.0.0",
             lifespan    = CourseInfoAPI.lifespan,
+        )
+
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins = ["*"],
+            allow_headers = ["*"],
+            allow_methods = ["GET"]
         )
 
         app.include_router(courses_router, prefix="/course-info-api")
